90014bfd5b2a0a909d83cca93ae14fde2a146347
wisteria-tiger <dartingsparrow77@icloud.com>
2026-03-26 10:50:53 -0500

    stuff

---
+++ main.py
@@ -0,0 +1,15 @@
+print("hello")
+
+myvariable = 5
+
+def myfunction(x, y):
+    return (x + y)
+
+print(myfunction(myvariable, 10))
+
+mylist = [1,2,3,4,5,"bot"]
+
+print(mylist[0])
+
+for x in mylist:
+    print(x)
