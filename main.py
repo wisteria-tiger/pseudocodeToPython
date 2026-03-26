c9ae7a39bd7786d49c3662ae4e8c40db964e2110
wisteria-tiger <dartingsparrow77@icloud.com>
2026-03-26 11:12:01 -0500

    more stuff

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+#from curses.ascii import isdigit
+
+
 print("hello")
 
 myvariable = 5
@@ -13,3 +16,23 @@ print(mylist[0])
 
 for x in mylist:
     print(x)
+
+#1
+word = input("Enter a string: ")
+print(word.upper())
+
+#2
+num1 = input("Enter a number: ")
+#while not isdigit:
+  #  print("Not a valid number.")
+  #  num1 = input("Enter a number: ")
+int(num1)
+num2 = input("Enter another number: ")
+#while not isdigit:
+   # print("Not a valid number.")
+   # num2 = input("Enter another number: ")
+int(num2)
+sum = num1 + num2
+print("Sum of numbers: " + sum)
+
+#3
